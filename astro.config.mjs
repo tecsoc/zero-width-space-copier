07c1d5e711d07b852df90e71b9c42b26547d9bd6
t-tecsoc <25559838+t-tecsoc@users.noreply.github.com>
2023-11-05 08:04:29 +0900

    feat: introduce google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
+import react from "@astrojs/react";
 import { defineConfig } from 'astro/config';
 
-import react from "@astrojs/react";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react(), partytown({
+    config: {
+      forward: ['dataLayer.push'],
+    },
+  })]
 });
